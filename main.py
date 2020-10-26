975ab58cb52e4c8e965355f9f6c73bf1b911d546
Di Ye <32302461+diye2@users.noreply.github.com>
2020-10-26 16:49:00 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from read_input import read_eqns, read_spc, read_def
+path = 'isoprene_oxidation_model_v5_190415'
+eqn_file = path+'/isoprene_full_v5.eqn'
+spc_file = path+'/isoprene_full_v5.spc'
+def_file = path+'/isoprene_full_v5.def'
+equations = read_eqns(eqn_file)
+species = read_spc(spc_file)
+inits = read_def(def_file)
